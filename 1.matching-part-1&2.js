8b829ba3626dc7b0f61fb0347635161291bbbd08
Phil <happy.melodist@gmail.com>
2021-12-11 23:08:50 -0600

    complete more RegEx training

--- 1.matching-part-1&2.js
+++ 1.matching-part-1&2.js
@@ -4,7 +4,7 @@
 my code below
 */
 
-let sentence = "The Quick brown fox jumps over the lazy dog";
+// let sentence = "The Quick brown fox jumps over the lazy dog";
 
 // the first example -- matching a word in a sentence (case-sensitive) (the test method)
 
@@ -55,6 +55,41 @@ let sentence = "The Quick brown fox jumps over the lazy dog";
 
 // example eight -- finds which letters of the alphabet with the [] utilizing the match() method
 
-let boobie8X = /[a-z]/gi;
-let search8Result = sentence.match(boobie8X);
-console.log(search8Result); // produces an array with all the letters of the alphabet found inside of sentence -- in order of appearance
+// let boobie8X = /[a-z]/gi;
+// let search8Result = sentence.match(boobie8X);
+// console.log(search8Result); // produces an array with all the letters of the alphabet found inside of sentence -- in order of appearance
+
+let sentence =
+  "The 11 Quick 65 brown 7210 fox 6054 jumps 32 over 205 the 51 lazy 103 dog really reallly";
+
+// example number nine getting the matched numbers and characters of the alphabet using the [] and the match() method
+
+// let boobie9X = /[1-3x-z]/gi;
+// let search9Result = sentence.match(boobie9X);
+// console.log(search9Result); // produces an array of ALL the characters in the string matching the request without regard for capitalization
+
+// example number ten -- getting the letters and numbers that don't match [] while utulizing the match method -- negated character sets -- we're using the caret ^ here
+
+// let boobies10X = /[^1-9a-v ]/gi;
+// let search10Result = sentence.match(boobies10X);
+// console.log(search10Result); // produces an array with the characters NOT included in the regular expression
+
+// example number eleven -- getting the matched numbers or characters that occur one or more times
+
+// let boobies11X = /l+/g;
+// let search11Result = sentence.match(boobies11X);
+// console.log(search11Result); // grabs and produces an array of the occurances of the searched-for character that happen one or more times (without the g it would return the first one only. without the plus sign, it would produce an array of each occurance)
+
+// example number twelve -- getting the matched characters that happen zero or more times
+
+let sentence2 = "gooooooooogle";
+
+// let boobies12X = /go*/;
+// let search12Result = sentence2.match(boobies12X);
+// console.log(search12Result); // grabs all of the occurances of the request no matter how many oooooooo
+
+// example number 13 -- get all of the matched characters with lazy matching
+
+let boobies13X;
+let search13Result = sentence2.match(boobies13X);
+console.log(search13Result);
